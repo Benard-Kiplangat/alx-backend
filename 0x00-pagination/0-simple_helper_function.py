eee7cd72e028813671d24510ceb1e3ad67b71663
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-08-19 20:46:42 +0300

    Removed 0-main.py and updated 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,18 @@
 """
 A simple helper function
 """
-def index_range(page, page_size):
-  baby_names_url = "https://s3.amazonaws.com/alx-intranet.hbtn.io/uploads/misc/2020/5/7d3576d97e7560ae85135cc214ffe2b3412c51d7.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARDDGGGOUSBVO6H7D%2F20240819%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240819T172609Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=83f8a0654fe3a4869b402ee5dbfb0c7096fbfdf5ab00ac030cc6db20e5c73b23"
+from typing import Tuple
 
-  d
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculates start index and an end index corresponding to the range of
+    indexes to return in a list
+
+    attr: page <int> - the page number
+          page_size <int> - the size of each page
+    
+    Return: Tuple of integers of size two
+    """
+    nextPageStartIndex = page * page_size
+    return nextPageStartIndex - page_size, nextPageStartIndex
